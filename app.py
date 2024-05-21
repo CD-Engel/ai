806bc6446327e25b3cfa8aacf047482b3f3c761d
grasshoff <gerd.grasshoff@gmail.com>
2024-05-21 10:27:12 +0200

    w

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import streamlit as st
+from streamlit_option_menu import option_menu
 from groq import Groq
 from vorlesungtools import Instruct
 
@@ -9,6 +10,11 @@ instruct = Instruct()
 
 init_values = {'text': '', 'response': ''}
 st.session_state.update({key: st.session_state.get(key, value) for key, value in init_values.items()})
+
+with st.sidebar:
+    selected = option_menu("", ["AI", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
 
 st.session_state['text'] = st.text_area("Geben Sie hier Ihren Text ein", st.session_state['text'])
 sys_msg="you are an assistant. Answer exactly what the user asks and do not provide additional information. You answer in English is elegant without exageration."
