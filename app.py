3e9a4320602587f5079e2271a327f9011dc127fe
grasshoff <gerd.grasshoff@gmail.com>
2024-05-21 08:32:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,12 +16,12 @@ def instruction(sys_mess,instruct,model="llama3-70b-8192"):
     )
     return message.choices[0].message.content
 
-init_values = {'text': 'input instruct', 'response': ''}
+init_values = {'text': 'Test: Wie hiess die zweite Ehefrau von Leonhard Euler', 'response': ''}
 st.session_state.update({key: st.session_state.get(key, value) for key, value in init_values.items()})
 
 st.session_state['text'] = st.text_area("Instruktion", st.session_state['text'])
-
+sys_msg = "You are a diligent assistant. Answer the instructions exactly as they are given. Do not invent answers."
 if st.session_state['text']:
-    st.session_state['response'] = instruction("Systemnachricht", st.session_state['text'])
+    st.session_state['response'] = instruction(sys_msg, st.session_state['text'])
 
 st.text_area("Antwort", st.session_state['response'])
