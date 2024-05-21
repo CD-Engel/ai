74c1c1f36a1c1e5eb3e69856fa60cc0ec1b7b0d0
grasshoff <gerd.grasshoff@gmail.com>
2024-05-21 08:47:47 +0200

    class

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
-import streamlit as st
 import os
+import streamlit as st
 from groq import Groq
+from vorlesungtools import Instruct
 
-def instruction(sys_mess,instruct,model="llama3-70b-8192"):
-    api_key = os.getenv("GROQ_API_KEY")
-    client = Groq(api_key=api_key)
-    message = client.chat.completions.create(
-        messages=[
-            {"role": "system", "content":sys_mess},
-            {"role": "user", "content": instruct}
-        ],
-        model=model,
-        temperature=0.0,
-        response_format={"type": "text"},
-    )
-    return message.choices[0].message.content
 
-init_values = {'text': 'Test: Wie hiess die zweite Ehefrau von Leonhard Euler, und wann lebte sie?', 'response': ''}
+# Initialisierung der Klasse
+instruct = Instruct()
+
+init_values = {'text': '', 'response': ''}
 st.session_state.update({key: st.session_state.get(key, value) for key, value in init_values.items()})
 
-st.session_state['text'] = st.text_area("Instruktion", st.session_state['text'])
-sys_msg = "You are a diligent assistant. Answer the instructions exactly as they are given. Do not invent answers."
+st.session_state['text'] = st.text_area("Geben Sie hier Ihren Text ein", st.session_state['text'])
+sys_msg="you are an assistant. Answer exactly what the user asks and do not provide additional information. You answer in English is elegant without exageration."
 if st.session_state['text']:
-    st.session_state['response'] = instruction(sys_msg, st.session_state['text'])
+    st.session_state['response'] = instruct.instruction("sys_msg", st.session_state['text'])
 
 st.text_area("Antwort", st.session_state['response'])
---
+++ vorlesungtools.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import Groq
+
+class Instruct:
+    def __init__(self, model="llama3-70b-8192"):
+        self.model = model
+        self.api_key = os.getenv("GROQ_API_KEY")
+        self.client = Groq(api_key=self.api_key)
+
+    def instruction(self, sys_mess, instruct):
+        message = self.client.chat.completions.create(
+            messages=[
+                {"role": "system", "content": sys_mess},
+                {"role": "user", "content": instruct}
+            ],
+            model=self.model,
+            temperature=0.0,
+            response_format={"type": "text"},
+        )
+        return message.choices[0].message.content
